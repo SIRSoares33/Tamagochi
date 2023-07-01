4b171233665053591746e33f790cd6df03059e2e
SIRSoares33 <sirsoares4@gmail.com>
2023-07-01 17:45:21 -0300

    :sparkles: Revisando conceitos de classes, e documentando o código.

--- Program.cs
+++ Program.cs
@@ -10,23 +10,23 @@ class Program
     {
         BichoVirtual bichoVirtual;
 
-        while (true)
+        while (true) // Bloco 1 -> Onde é criado o tamagochi
         {
             try
             {
+                Console.WriteLine();
                 Console.Clear();
                 Console.WriteLine("Vamos criar seu tamagoshi!");
                 Console.WriteLine();
 
                 Console.Write("Nome do seu tamagoshi: ");
-                string? name = Console.ReadLine();
+                string? name = Console.ReadLine(); 
 
                 Console.Write($"Cor de {name}: ");
                 Cor cor = Enum.Parse<Cor>(Console.ReadLine());
 
 
-                bichoVirtual = new BichoVirtual(name, cor, 85, 0, 85);
-
+                bichoVirtual = new BichoVirtual(name, cor); // Instancia um bicho virtual com 85 de fome, 0 de felicidade e 85 de sono
 
                 Console.WriteLine("Perfeito! Você criou seu tamagoshi.");
 
@@ -34,7 +34,7 @@ class Program
 
                 break;
             }
-            catch (ArgumentException)
+            catch (ArgumentException) // Caso o user digite a cor errada no input "cor" o programa pega essa exceção
             {
                 Console.WriteLine("Puxa! parece que você digitou a cor errada. Vamos tentar novamente.");
                 Console.ReadKey();
@@ -42,48 +42,51 @@ class Program
         }
 
 
-        while (true)
+        while (true) // Bloco 2 -> Onde o jogo começa
         {
             try
             {
                 Console.Clear();
 
-                if (Morte.VerificarMorte(bichoVirtual))
+                if (Funcionalidades.VerificarMorte(bichoVirtual)) // Este método avalia se o tamagochi morreu, considerando a fome e o sono enm 100%.
                 {
                     Console.WriteLine($"{bichoVirtual.Name} morreu. Ou você não alimentou, ou não o colocou para dormir.");
-                    break;
+                    break; // se a expressão for verdadeira isto fecha o programa.
                 }
-                   
 
-                Console.WriteLine($"Informações sobre {bichoVirtual.Name}:");
+                
+
+                Console.WriteLine($"Informações sobre {bichoVirtual.Name}:");  
                 Console.WriteLine();
-                Console.WriteLine(bichoVirtual.ToString());
+                Console.WriteLine(bichoVirtual.ToString()); // Isto é o status do tamagochi. Aparece a % de fome, felicidade e sono aqui.
 
                 Console.WriteLine();
 
                 Console.WriteLine($"O que deseja fazer com {bichoVirtual.Name}? ");
 
-                Console.WriteLine(Tela.ExibirPossibilidadesDeInteracao());
+                Console.WriteLine(Tela.ExibirPossibilidadesDeInteracao()); // Este método informa ao jogador o que digitar para interagir com o tamagochi
 
                 Console.Write("O que você deseja fazer? ");
                 char interaction = char.Parse(Console.ReadLine().ToLower());
 
                 Console.Clear();
 
-                Console.WriteLine(BichoVirtual.Interacao(bichoVirtual, interaction));
+                Console.WriteLine(Funcionalidades.Interacao(bichoVirtual, interaction)); // este método recebe um char que significa a interação com o tamagochi.
+                // ele tem o objetivo de verificar qual interação foi escolhida, e executa-la.
 
                 Console.ReadKey();
             }
 
-            catch (FeelingException error)
+            catch (FeelingException error) // Feeling Exception é o tratamento de erro caso o tamagochi já esteja satifeito da fome ou sono 
+            // (fome = 0, sono = 0)
             {
                 Console.WriteLine(error.Message);
                 Console.ReadKey();
             }
-            catch (InteractionException)
+            catch (InteractionException) // É O tratamento de erro caso o user não digite o char que é possível digitar (b, d, f)
             {
             }
-            catch (FormatException)
+            catch (FormatException) // é o tratamento de erro caso o user não digite um char
             {
             }
         }
---
+++ tamagoshi/Funcionalidades.cs
@@ -0,0 +1,37 @@
+﻿using telaExceptions;
+
+namespace tamagoshi
+{
+    public class Funcionalidades
+    {
+        public static string? Interacao(BichoVirtual bichoVirtual, char interaction) // esse método estático recebe um char e um tamagochi para realizar
+        // a interação CLIENT -> SERVER / SERVER -> CLIENT.
+        {
+            switch (interaction)
+            {
+                case 'b':
+                    bichoVirtual.Brincar();
+                    return $"Você brincou com {bichoVirtual.Name}. Ele recebeu mais 5 de felicidade.";
+
+                case 'd':
+                    bichoVirtual.Dormir();
+                    return $"{bichoVirtual.Name} Adormeceu. Energia recuperada em 100%";
+
+
+                case 'f':
+                    bichoVirtual.Comer();
+                    return $"{bichoVirtual.Name} comeu toda a refeição. Isso saciou 100% de sua fome";
+
+                default:
+                    throw new InteractionException(null);
+            }
+        }
+        public static bool VerificarMorte(BichoVirtual bicho)
+        {
+            if (bicho.Fome >= 100 || bicho.Sono >= 100)
+                return true;
+
+            return false;
+        }
+    }
+}
